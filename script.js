d3af91225db2be9a1bb6e618d733d40103bb5688
zYumoto <victorjuanrazer@gmail.com>
2023-11-22 12:17:20 -0300

    add select camera

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const cameraSelect = document.getElementById("camera-select");
+
 document.addEventListener('DOMContentLoaded', function () {
     let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 
@@ -8,6 +10,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
+            // Preenche as opções da caixa de seleção com as câmeras disponíveis
+            cameras.forEach(function (camera, index) {
+                const option = document.createElement('option');
+                option.value = index;
+                option.text = camera.name || `Câmera ${index + 1}`;
+                cameraSelect.add(option);
+            });
+
+            // Adiciona um evento de mudança à caixa de seleção
+            cameraSelect.addEventListener('change', function () {
+                const selectedCameraIndex = cameraSelect.value;
+                const selectedCamera = cameras[selectedCameraIndex];
+                scanner.start(selectedCamera);
+            });
+
+            // Inicia o scanner usando a primeira câmera por padrão
             scanner.start(cameras[0]);
         } else {
             console.error('Nenhuma câmera encontrada.');
